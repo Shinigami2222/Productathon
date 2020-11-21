540d36a9f58329661db9d0deeab193b06402375d
Utkarsh_190 <utkarshtyagi62@gmail.com>
2020-11-22 01:42:09 +0530

    map simulation added

--- map_change.js
+++ map_change.js
@@ -5,10 +5,19 @@ function displayNextImage() {
 
 
 function startTimer() {
-    setInterval(displayNextImage, 1000);
+    setInterval(displayNextImage, 500);
 }
 
 var images = [], x = -1;
-images[0] = "images/img1.jfif";
-images[1] = "images/img2.jfif";
-images[2] = "images/img3.jfif";
+images[0] = "images/img1.jpeg";
+images[1] = "images/img2.jpeg";
+images[2] = "images/img3.jpeg";
+images[3] = "images/img4.jpeg";
+images[4] = "images/img5.jpeg";
+images[5] = "images/img6.jpeg";
+images[6] = "images/img7.jpeg";
+images[7] = "images/img8.jpeg";
+images[8] = "images/img9.jpeg";
+images[9] = "images/img10.jpeg";
+images[10] = "images/img11.jpeg";
+images[11] = "images/img12.jpeg";
